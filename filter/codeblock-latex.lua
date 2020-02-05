bf8b069a4b1f20f807465fa994c58f642e4dc4b2
taha@asks2 <taha@chepec.se>
2020-02-05 16:49:49 +0100

    Improved the layout of tcolorbox with optional display of title
    by simply abusing the codeblock "language" string for this purpose.
    
    Also adjusted vskip above and below to make the text following each blockquote
    visually connected to the preceding blockquote.

--- filter/codeblock-latex.lua
+++ filter/codeblock-latex.lua
@@ -3,7 +3,19 @@ if FORMAT:match 'latex' then
     function CodeBlock (block)
       -- Replace all codeblocks with our manuscript environment
       -- https://tex.stackexchange.com/a/511575/10824
-      return pandoc.RawBlock('tex', '\\begin{manuscript}\n' .. block.text .. '\n\\end{manuscript}')
+      -- my very naive solution to have whatever the user places in the codeblock
+      -- "language" definition to show up in the tcolorbox title
+      if block.classes[1] == nil then
+        return pandoc.RawBlock(
+          'tex', 
+          '\\begin{manuscriptnopage}\n' .. block.text .. '\n\\end{manuscriptnopage}'
+        )
+      else
+        return pandoc.RawBlock(
+          'tex',
+          '\\begin{manuscript}{' .. block.classes[1] .. '}\n' .. block.text .. '\n\\end{manuscript}'
+        )
+      end
     end
 end
 
